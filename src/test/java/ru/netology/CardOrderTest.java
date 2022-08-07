bba823a630b81d16c58a12d7b95651fe23406839
4Shire <t-grishe@yandex.ru>
2022-08-07 15:55:25 +0300

    First commit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    public void shouldTest(){
+        open("http://localhost:7777");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иван Дулин");
+        form.$("[data-test-id=phone] input").setValue("+79999999999");
+        form.$("[data-test-id=agreement").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
